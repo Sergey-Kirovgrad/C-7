2cec01f08b262ccaf16db6775f120e06635b204f
Sergey <arakcheev1@yandex.ru>
2023-01-02 17:17:33 +0500

    add zadacha50

---
+++ Zadacha50/Program.cs
@@ -0,0 +1,46 @@
+﻿//Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+//и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+Console.WriteLine("Введите номер строки позиции элемента");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите номер столбца позиции элемента");
+int m = int.Parse(Console.ReadLine());
+int[,] numbers = new int[10, 10];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+if (n > numbers.GetLength(0) || m > numbers.GetLength(1))
+{
+    Console.WriteLine("Такого элемента нет");
+}
+else
+{
+    Console.WriteLine($"Значение элемента {n} строки и {m} столбца равно {numbers[n-1,m-1]}");
+}
+
+PrintArray(numbers);
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        {        
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array [i,j] = new Random().Next(-100, 100)/10;
+            }   
+        }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($" {array[i, j]} ");
+        }
+        
+        Console.WriteLine();
+    }
+}    
